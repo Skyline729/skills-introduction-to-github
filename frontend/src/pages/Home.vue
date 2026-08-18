9c152e212f6d670794813599f76970c28835fd15
Skyline729 <3417457651@qq.com>
2026-08-18 16:45:52 +0800

    feat: add Yu Xiaobao homepage prototype

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>芋小宝</h1>
+    <p>新中式甜品生活品牌</p>
+    <button>探索限定甜品</button>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.home {
+  min-height: 600px;
+  padding: 80px;
+  text-align: center;
+}
+</style>
