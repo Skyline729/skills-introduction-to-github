292500da60512d482de9683aeaf62c2c48176d07
Skyline729 <3417457651@qq.com>
2026-08-18 16:52:28 +0800

    feat: add membership system page for Yu Xiaobao mall

---
+++ frontend/src/pages/Membership.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="membership">
+    <h1>芋小宝会员计划</h1>
+    <ul>
+      <li>消费积分</li>
+      <li>限定礼盒优先购买</li>
+      <li>会员专属福利</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+</script>
