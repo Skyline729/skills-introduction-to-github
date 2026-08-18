8cdaefcb05ccc92c080efd6f67d18d65de9950e7
Skyline729 <3417457651@qq.com>
2026-08-18 16:45:57 +0800

    feat: add product card component

---
+++ frontend/src/components/ProductCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="product-card">
+    <h3>{{ product.name }}</h3>
+    <p>{{ product.description }}</p>
+    <span>¥{{ product.price }}</span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  product: Object
+})
+</script>
