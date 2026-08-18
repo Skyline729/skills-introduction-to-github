86981498f890d61e0a9e8cc75fe67bebb68adf8a
Skyline729 <3417457651@qq.com>
2026-08-18 16:51:38 +0800

    feat: add Yu Xiaobao product list page

---
+++ frontend/src/pages/Products.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="products-page">
+    <h1>芋小宝甜品商城</h1>
+    <p>精选东方甜品与限定礼盒</p>
+    <div class="products">
+      <div v-for="item in products" :key="item.name" class="card">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+        <strong>¥{{ item.price }}</strong>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { products } from '../data/products'
+</script>
